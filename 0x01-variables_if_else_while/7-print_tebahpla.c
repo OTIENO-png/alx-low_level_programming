8e716d7895a7ab8c19e2700c0223aa8e3ad38942
brian <otienolubalo@yahoo.com>
2022-09-09 23:19:41 +0300

    solved question 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+    int a;
+    for(a == "z";a => "a"; a--)
+    {
+        putchar(a);
+    }
+    
+    putchar("/n");
+    return(0)
+}
