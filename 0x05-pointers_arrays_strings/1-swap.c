4546a20aa01aee45009c17b293c061b60d7ca600
brian <otienolubalo@yahoo.com>
2022-09-20 22:27:46 +0300

    commented on other lines

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/**
- * *swap-int - swaps the avalues of a and b
- *  *@b:pointer 2
- *  *@a:pointer 1
- *      *Return 0
- */
+// /**
+//  * *swap-int - swaps the avalues of a and b
+//  *  *@b:pointer 2
+//  *  *@a:pointer 1
+//  *      *Return 0
+//  */
 void swap_int(int *a, int b)
 {
     *a += *b;
